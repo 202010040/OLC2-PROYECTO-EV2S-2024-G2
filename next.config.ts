67097e1b244e771f8742c56849dffff6a27f8087
202010040 <3772166310101@ingenieria.usac.edu.gt>
2024-12-06 18:21:36 -0600

    actions prueba 7

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     output: 'export',
-    basePath: '/front-end-app'
+    basePath: '/OLC2-PROYECTO-EV2S-2024-G2',
+    trailingSlash: true, // Opcional, pero útil para GB pages
 };
 
 export default nextConfig;
