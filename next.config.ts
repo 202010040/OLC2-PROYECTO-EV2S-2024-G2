f8ad10c84e508a4fb706937e8e58c1cb9e853b39
202010040 <3772166310101@ingenieria.usac.edu.gt>
2024-12-06 18:58:01 -0600

    Git Actions Prueba 10

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,23 @@
 import type { NextConfig } from "next";
 
+/*
 const nextConfig: NextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   trailingSlash: true,
   publicRuntimeConfig: {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  },
+};
+
+export default nextConfig;
+*/
+
+// Produccion
+const nextConfig: NextConfig = {
+  basePath: '/OLC2-PROYECTO-EV2S-2024-G2',
+  trailingSlash: true,
+  publicRuntimeConfig: {
+    basePath: '/OLC2-PROYECTO-EV2S-2024-G2',
   },
 };
 
